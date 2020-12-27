42ea40750f7c19f868cbc26dc8101e83d94c37b8
Roger Fernandes <roger.afernandes15@gmail.com>
2020-12-27 06:25:51 -0300

    registration validation with yup

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import {
   FiMail, FiLock, FiUser, FiArrowLeft,
 } from 'react-icons/fi';
-
 import { Form } from '@unform/web';
-
+import * as Yup from 'yup';
 import logoImg from '../../assets/logo.svg';
-
 import Input from '../../components/Input';
-
 import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
 const Signup: React.FC = () => {
-  function handleSubmit(data: any):void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: any) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string().required('Email obrigatório').email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: true,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   return (
     <Container>
